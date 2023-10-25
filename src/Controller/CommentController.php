e8241bae02e751ac783b80c0056379aa7d4602d6
kaisyQ <rutyo8@mail.ru>
2023-10-25 13:29:17 +1000

    comment rep/service and category entity

---
+++ src/Controller/CommentController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use App\Service\CommentService;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route(name: "comments", path: "api/comment")]
+class CommentController extends AbstractController {
+
+    public function __construct(
+        private CommentService $commentService,
+    ) {}
+
+    #[Route(path: "/", name: "index", methods: [])]
+    public function index(Request $request) : Response {
+        return $this->json($this->commentService->getComments());
+    }
+
+    public function show() {
+    }
+    public function store(Request $request) {
+    }
+    public function update(Request $request, $id) { 
+    }
+    public function destroy(Request $request, $id) {
+    }
+}
---
+++ src/Service/CommentService.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Service;
+ 
+class CommentService {
+
+    public function getComments() {
+    }
+    public function createComment(){
+
+    }
+    public function deleteCommentById($id){
+    }
+    public function getCommentById($id){
+    }
+    public function updateCommentById($id, $data){
+    }
+}
