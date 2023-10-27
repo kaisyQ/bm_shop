2d44eb1dd53838c7ccd280832e77a74c33de66a6
kaisyQ <rutyo8@mail.ru>
2023-10-27 14:22:23 +1000

    comment controller setup

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -9,23 +9,42 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route(name: "comments", path: "api/v1/comments")]
-class CommentController extends AbstractController {
+class CommentController extends AbstractController
+{
 
     public function __construct(
         private CommentService $commentService,
-    ) {}
+    ) {
+    }
 
-    #[Route(path: "/", name: "index", methods: [])]
-    public function index(Request $request) : Response {
+    #[Route(path: "/", name: "comment_index", methods: ["GET"])]
+    public function index(Request $request): Response
+    {
         return $this->json($this->commentService->getComments());
     }
 
-    public function show() {
+
+    #[Route(path: "/{id}", name: "comment_show", methods: ["GET"])]
+    public function show(int $id)
+    {
+        return $this->json(["id" => $id]);
     }
-    public function store(Request $request) {
+
+    #[Route(path: "/create", name: "comment_store", methods: ["POST"])]
+    public function store(Request $request): Response
+    {
+        return $this->json(["id" => 1]);
     }
-    public function update(Request $request, $id) { 
+
+    #[Route(path: "/update/{id}", name: "comment_update", methods: ["PUT"])]
+    public function update(Request $request, int $id): Response
+    {
+        return $this->json(["id" => $id]);
     }
-    public function destroy(Request $request, $id) {
+
+    #[Route(path: "/delete/{id}", name: "comment_destroy", methods: ['DELETE'])]
+    public function destroy(Request $request, int $id): Response
+    {
+        return $this->json(["id" => $id]);
     }
 }
